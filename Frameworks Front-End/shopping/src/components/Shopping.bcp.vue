96bf033914fbf26ac235df36c168c1c97891cc58
Wellington Nascimento <wnsferreira@gmail.com>
2022-02-16 02:05:56 -0300

    add projetos Vue

---
+++ Frameworks Front-End/shopping/src/components/Shopping.bcp.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+        <h1>{{ name_store || "My store" }}</h1>
+        <h2 v-html="subtitle"></h2>
+
+        <button
+            @click="insert = !insert">Inserir</button>
+        <div v-show="insert">
+            <input type="text"
+            @keyup.enter="item.push({ ...newItem })"
+            v-model="newItem.product">
+
+            <strong>Priority:</strong>
+            <label>
+                <input type="radio" v-model="newItem.priority"> value="high">High
+            </label>
+            <label>
+                <input type="radio" v-model="newItem.priority"> value="low">Low
+            </label>
+            <label>
+                <strong>Color:</strong>
+                <select v-model="newItem.color">
+                    <option value="black">Black</option>
+                    <option value="blue">Blue</option>
+                    <option value="pink">Pink</option>
+                </select>
+            </label>
+            
+            <label>
+                <strong>Gift:</strong>
+                <input type="checkbox" v-model="newItem.gift">
+                Is a gift?
+            </label>
+
+            <label>
+                <strong>Complements:</strong>
+                <input type="checkbox" v-model="newItem.complements" value="giftcard">
+                Gift card?
+                <input type="checkbox" v-model="newItem.complements" value="tracking">
+                Tracking?
+            </label>
+
+            <br>
+            <button
+                :disabled="newItem.product.lenght < 3"
+                @click="items.push({ ...newItem })">
+                Save item
+            </button>
+
+        </div>
+
+        <h3>Itens
+            <span v-show="items.length > 0">
+                {{ items.lenght }}
+            </span>
+        </h3>
+
+        <p v-if="items.length === 0">No items</p>
+        <div v-else-if="items.length === 1"> 1 Item</div>
+        <span v-else>{{ items.length }} Items</span>
+
+        <ul>
+            <li
+                v-for='item in items'
+                :key='item.id'>
+                <img v-bind:src="item.img" width="56">
+                {{ item.product }}
+                {{ item.price }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Shopping',
+        props: {
+            name_store: String,
+        },
+        data() {
+            return {
+                subTitle: "<strong>Mult</strong> Shopping",
+                insert: false,
+                newItem: {
+                    product: '',
+                    priority: 'low',
+                    color: 'black',
+                    gift: false,
+                    complements: [],
+                },
+                Items: [
+                    {id:1, price: 60.0, img: "", product: 'Chapéu'},
+                    {id:2, price: 100.0, img: "", product: 'Jogo'},
+                    {id:3, price: 20.0, img: "", product: 'Caneca'},
+                ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    strong, label{
+        margin: 10px auto;
+        display: block;
+    }
+    h3{
+        margin: 40px 0 0;
+    }
+    ul{
+        list-style-type: none;
+        padding: 0;
+    }
+    li{
+        display: block;
+        margin: 10 px 10 px;
+        font-size: x-large;
+    }
+    a{
+        color: #42b983
+    }
+</style>
