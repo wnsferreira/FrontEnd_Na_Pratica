28543b52deedd19766326b901bf0bd18a3887a25
Wellington Nascimento <wnsferreira@gmail.com>
2022-03-04 19:48:45 -0300

    add flexbox

---
+++ Frameworks Front-End/TP1/tp1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Questao10 msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Questao10 from './components/Questao10.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Questao10
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
